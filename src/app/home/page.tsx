f04ab807b6638c054ec31f609849d5f1ad552f0e
K Kevin Gladson <kevingladson2427@gmail.com>
2025-09-03 21:13:49 +0530

    All the tests failed

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+export default async function Home() {
+
   return (
     <div>
-      <h1>Vibes. Anytime. Anywhere.</h1>
+      <h1>🎶 Your Top Spotify Tracks</h1>
+      <ul>
+        {/* {data.items?.map((track: any) => (
+          <li key={track.id}>
+            {track.name} — {track.artists.map((a: any) => a.name).join(", ")}
+          </li>
+        ))} */}
+      </ul>
     </div>
   );
 }
